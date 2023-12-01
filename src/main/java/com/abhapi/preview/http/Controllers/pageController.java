9b5ff7b3a653a095b369f6f440562d00f1c747c7
Afreed Bin Haque <69814471+afreed-bin-haque@users.noreply.github.com>
2023-12-01 22:07:22 +0600

    Index page creation

--- src/main/java/com/abhapi/preview/http/Controllers/pageController.java
+++ src/main/java/com/abhapi/preview/http/Controllers/pageController.java
@@ -1,13 +1,23 @@
 package com.abhapi.preview.http.Controllers;
 
+import java.util.Map;
+import java.util.HashMap;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class pageController {
 
   @GetMapping("/")
-  public String welcomeRes(){
-    return "Welcome to abh api platform";
+  @ResponseBody
+  public Map<String, String>  welcomeRes(){
+    Map<String, String> response = new HashMap<>();
+    response.put("msg", "Welcome to abh api platform");
+    response.put("version", Float.toString(1.0f));
+    response.put("platform", "Java Spring Boot");
+    response.put("status", Boolean.toString(true));
+    return response;
   }
 }
